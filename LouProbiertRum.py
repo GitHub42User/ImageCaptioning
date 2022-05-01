9047fe3e7a60a16b616f33608b3a37ea180ec048
GitHub42User <92952292+GitHub42User@users.noreply.github.com>
2022-05-01 16:34:07 +0200

    Update LouProbiertRum.py

--- LouProbiertRum.py
+++ LouProbiertRum.py
@@ -5,8 +5,7 @@ import numpy as np
 st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATA_URL = ('http://3.bp.blogspot.com/-6uKj8avN8oc/UsvAhUlpeSI/AAAAAAAACL8/ce31UUzapow/w1200-h630-p-k-no-nu/Peugeot+308+Sedan2.jpg')
 
 @st.cache
 def load_data(nrows):
