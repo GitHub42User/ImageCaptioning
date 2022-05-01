63684a76a9a1059a33425bd3728b5615015add11
GitHub42User <92952292+GitHub42User@users.noreply.github.com>
2022-05-01 17:44:31 +0200

    Update LouProbiertRum.py

--- LouProbiertRum.py
+++ LouProbiertRum.py
@@ -1,18 +1,15 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+st.title('Image Captioning Group 13')
 
 st.expander("The Team"):
-st.expander("The Task"):
+  st.write("Hello, we are Leon Lang, Jean Luis Fichtner and Loredana Bratu and we are the creators of the app")
+st.expander("The Mission"):
+  st.write("Our app’s aim is to automatically describe an image with one or more natural language sentences. To generate textual description of images we will be using Neural Network and Deep Learning Techniques.")
 st.expander("The Dataset"):
-st.write("""
-# Image Captioning Group 13
-Hello, we are Leon Lang, Jean Luis Fichtner and Loredana Bratu and we are the creators 
-of the app ,,Image captioning“. Our app’s aim is to automatically describe an image with 
-one or more natural language sentences. To generate textual description of images we will 
-be using Neural Network and Deep Learning Techniques.
-""")
+  st.write("Here you can swipe through some examples from our Dataset")
 
-st.write("Here you can swipe through some examples from our Dataset")
+
 
 st.image('https://img.alicdn.com/imgextra/i3/817462628/O1CN01eLHBGX1VHfUMBA1du_!!817462628.jpg')
