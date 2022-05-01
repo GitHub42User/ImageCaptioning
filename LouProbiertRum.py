e2b94fe6765248eae604c1059c2f60b5aa7eba19
GitHub42User <92952292+GitHub42User@users.noreply.github.com>
2022-05-01 15:54:10 +0200

    Update LouProbiertRum.py

--- LouProbiertRum.py
+++ LouProbiertRum.py
@@ -2,4 +2,27 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.write("""#Hello Loulilein""")
+st.title('Image Captioning')
+
+
+
+
+
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+
+
+
+
+
+streamlit run 1stONE.py
