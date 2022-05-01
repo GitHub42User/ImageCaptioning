c068707f0c723888d8d2cf96ccdfa3ae26e605de
GitHub42User <92952292+GitHub42User@users.noreply.github.com>
2022-05-01 17:50:32 +0200

    Update LouProbiertRum.py

--- LouProbiertRum.py
+++ LouProbiertRum.py
@@ -8,8 +8,19 @@ with st.expander("The Team"):
 with st.expander("The Mission"):
   st.write("Our app’s aim is to automatically describe an image with one or more natural language sentences. To generate textual description of images we will be using Neural Network and Deep Learning Techniques.")
 with st.expander("The Dataset"):
-  st.write("Here you can swipe through some examples from our Dataset")
+  st.write("Here you can see some examples from our Dataset")
 
+col1, col2, col3 = st.columns(3)
 
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
 
-st.image('https://img.alicdn.com/imgextra/i3/817462628/O1CN01eLHBGX1VHfUMBA1du_!!817462628.jpg')
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
+
