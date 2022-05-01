c03aeaefcc22fc93faef75f7ea3ada49d307e376
GitHub42User <92952292+GitHub42User@users.noreply.github.com>
2022-05-01 16:55:52 +0200

    Update LouProbiertRum.py

--- LouProbiertRum.py
+++ LouProbiertRum.py
@@ -8,7 +8,24 @@ import io
 import json
 
 
-r = http.request('GET', 'http://3.bp.blogspot.com/-6uKj8avN8oc/UsvAhUlpeSI/AAAAAAAACL8/ce31UUzapow/w1200-h630-p-k-no-nu/Peugeot+308+Sedan2.jpg' )
+routes = {}
+df = pd.read_csv('index_file.csv')
+
+
+# iterate over the rows and download the images
+for index, row in df.iterrows():
+    # get the image from the url
+    http = urllib3.PoolManager()
+    r = http.request('GET', row['url'])
     img_data = r.data
-  
-st.title('Uber pickups in NYC')
+    
+    # save data to image file and row['TEXT'] to json file
+    filename = "filename"
+    image = Image.open(io.BytesIO(img_data))
+    image.save(filename + '.png')
+    text = row['TEXT']
+    
+    # save to routes for later use
+    routes[filename] = text
+
+json.dump(routes, open("routes.json", "w"))
