aeb82681251b67fcb5d337658d3812fb5b4b5e71
GitHub42User <92952292+GitHub42User@users.noreply.github.com>
2022-05-01 17:45:28 +0200

    Update LouProbiertRum.py

--- LouProbiertRum.py
+++ LouProbiertRum.py
@@ -3,8 +3,7 @@ import pandas as pd
 import numpy as np
 st.title('Image Captioning Group 13')
 
-st.expander("The Team"):
-  st.write("Hello, we are Leon Lang, Jean Luis Fichtner and Loredana Bratu and we are the creators of the app")
+st.expander("The Team"):st.write("Hello, we are Leon Lang, Jean Luis Fichtner and Loredana Bratu and we are the creators of the app")
 st.expander("The Mission"):
   st.write("Our app’s aim is to automatically describe an image with one or more natural language sentences. To generate textual description of images we will be using Neural Network and Deep Learning Techniques.")
 st.expander("The Dataset"):
