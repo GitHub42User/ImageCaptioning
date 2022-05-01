82d4705ccab22364537b5b8ee4250581fe00de53
GitHub42User <92952292+GitHub42User@users.noreply.github.com>
2022-05-01 16:37:44 +0200

    Update LouProbiertRum.py

--- LouProbiertRum.py
+++ LouProbiertRum.py
@@ -2,8 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+from PIL import Image
+
 st.title('Uber pickups in NYC')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('http://3.bp.blogspot.com/-6uKj8avN8oc/UsvAhUlpeSI/AAAAAAAACL8/ce31UUzapow/w1200-h630-p-k-no-nu/Peugeot+308+Sedan2.jpg')
-
+image = Image.open('http://3.bp.blogspot.com/-6uKj8avN8oc/UsvAhUlpeSI/AAAAAAAACL8/ce31UUzapow/w1200-h630-p-k-no-nu/Peugeot+308+Sedan2.jpg')
+image.show()
