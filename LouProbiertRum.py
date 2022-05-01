f661db31a81d3b287efcbb4e86345b839bdd47cc
GitHub42User <92952292+GitHub42User@users.noreply.github.com>
2022-05-01 17:03:01 +0200

    Update LouProbiertRum.py

--- LouProbiertRum.py
+++ LouProbiertRum.py
@@ -4,5 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-img = mpimg.imread('http://3.bp.blogspot.com/-6uKj8avN8oc/UsvAhUlpeSI/AAAAAAAACL8/ce31UUzapow/w1200-h630-p-k-no-nu/Peugeot+308+Sedan2.jpg')
+
+img = mpimg.imread('https://github.com/GitHub42User/ImageCaptioning/blob/main/O1CN01eLHBGX1VHfUMBA1du_!!817462628.jpg')
 print(img)
