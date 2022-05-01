3dc30ab5fd73483049bb49059538c638a47a6305
GitHub42User <92952292+GitHub42User@users.noreply.github.com>
2022-05-01 17:05:10 +0200

    Update LouProbiertRum.py

--- LouProbiertRum.py
+++ LouProbiertRum.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 
 
-img = mpimg.imread('https://github.com/GitHub42User/ImageCaptioning/blob/main/O1CN01eLHBGX1VHfUMBA1du_!!817462628.jpg')
-print(img)
+<img.src=""/>
