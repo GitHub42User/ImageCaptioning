52cfe8044840cae997c2df964568f75e9c4cb2f2
GitHub42User <92952292+GitHub42User@users.noreply.github.com>
2022-05-01 17:17:27 +0200

    Update LouProbiertRum.py

--- LouProbiertRum.py
+++ LouProbiertRum.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from PIL import Image
 
-# creating a object
-im = Image.open('http://3.bp.blogspot.com/-6uKj8avN8oc/UsvAhUlpeSI/AAAAAAAACL8/ce31UUzapow/w1200-h630-p-k-no-nu/Peugeot+308+Sedan2.jpg')
-  
-im.show()
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://img.alicdn.com/imgextra/i3/817462628/O1CN01eLHBGX1VHfUMBA1du_!!817462628.jpg')
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
