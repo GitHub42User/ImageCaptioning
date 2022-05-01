9c3d572c0d2e80ffc63618c72656a7ec0a4fa066
GitHub42User <92952292+GitHub42User@users.noreply.github.com>
2022-05-01 17:18:07 +0200

    Update LouProbiertRum.py

--- LouProbiertRum.py
+++ LouProbiertRum.py
@@ -2,6 +2,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.write("""
+# Image Captioning Group 13
+Hello, we are Leon Lang, Jean Luis Fichtner and Loredana Bratu and we are the creators 
+of the app ,,Image captioning“. Our app’s aim is to automatically describe an image with 
+one or more natural language sentences. To generate textual description of images we will 
+be using Neural Network and Deep Learning Techniques.
+""")
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://img.alicdn.com/imgextra/i3/817462628/O1CN01eLHBGX1VHfUMBA1du_!!817462628.jpg')
