10dec28ce8c058006bfe4f8d26ac1623c81303b4
GitHub42User <92952292+GitHub42User@users.noreply.github.com>
2022-05-01 16:39:46 +0200

    Update LouProbiertRum.py

--- LouProbiertRum.py
+++ LouProbiertRum.py
@@ -2,9 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-from PIL import Image
 
+
+%pylab inline
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+img = mpimg.imread('your_image.png')
+imgplot = plt.imshow(img)
+plt.show()
 st.title('Uber pickups in NYC')
-
-image = Image.open('http://3.bp.blogspot.com/-6uKj8avN8oc/UsvAhUlpeSI/AAAAAAAACL8/ce31UUzapow/w1200-h630-p-k-no-nu/Peugeot+308+Sedan2.jpg')
-image.show()
