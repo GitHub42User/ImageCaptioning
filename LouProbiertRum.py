d69172ba6b94a2dbbe2ca4c40faf98a709c06b4f
GitHub42User <92952292+GitHub42User@users.noreply.github.com>
2022-05-01 17:24:59 +0200

    Update LouProbiertRum.py

--- LouProbiertRum.py
+++ LouProbiertRum.py
@@ -10,13 +10,4 @@ one or more natural language sentences. To generate textual description of image
 be using Neural Network and Deep Learning Techniques.
 """)
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://img.alicdn.com/imgextra/i3/817462628/O1CN01eLHBGX1VHfUMBA1du_!!817462628.jpg')
-
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
 st.image('https://img.alicdn.com/imgextra/i3/817462628/O1CN01eLHBGX1VHfUMBA1du_!!817462628.jpg')
