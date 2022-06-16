afeb4b061259f3a20e4e8eb9fbb9eecf76113477
GitHub42User <92952292+GitHub42User@users.noreply.github.com>
2022-06-17 01:14:39 +0200

    Update FinalTryApp.py

--- FinalTryApp.py
+++ FinalTryApp.py
@@ -451,5 +451,4 @@ with st.expander("Our vision..."):
 with st.expander("Here you can try our Image Captoning Program"):
   st.write("Please upload an image press the following Button.")
   image_url = st.file_uploader("Choose a file")
-  if st.button('Start now'): 
     
